dafd02b734d9417395153788e4a777cb59622ca5
Jan Moriaux <jan.moriaux@aclagro.be>
2019-03-26 16:59:38 +0100

    section 17 completed

---
+++ src/components/MovieFilter.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="movie-filter">
+    <h2>Filter Results</h2>
+    <h3>By time of day</h3>
+    <div class="filter-group">
+      <check-filter
+        category="time"
+        v-for="time in times"
+        v-bind:title="time"
+        v-on:check-filter="checkFilter"
+        v-bind:key="time"
+      ></check-filter>
+    </div>
+    <h3>By genre</h3>
+    <div class="filter-group">
+      <check-filter
+        category="genre"
+        v-for="genre in genres"
+        v-bind:title="genre"
+        v-on:check-filter="checkFilter"
+        v-bind:key="genre"
+      ></check-filter>
+    </div>
+  </div>
+</template>
+
+<script>
+import genres from "../util/genres";
+import times from "../util/times";
+import CheckFilter from "./CheckFilter.vue";
+
+export default {
+  data() {
+    return {
+      genres,
+      times
+    };
+  },
+  methods: {
+    checkFilter(category, title, checked) {
+      this.$emit("check-filter", category, title, checked);
+    }
+  },
+  components: {
+    CheckFilter
+  }
+};
+</script>
